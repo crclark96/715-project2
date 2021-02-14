3755be1ab11602bc0f75ec01430407cc4b38c66e
Collin Reilly Clark <crclark96@gmail.com>
2021-02-13 21:24:45 -0500

    add interrupt example

---
+++ examples/interrupt.rs
@@ -0,0 +1,78 @@
+#![no_std]
+#![no_main]
+#![feature(abi_avr_interrupt)]
+
+// heavily influenced by the safe interrupts described by this blogpost:
+// https://blog.rahix.de/005-avr-hal-millis/
+// https://github.com/Rahix/avr-hal/blob/master/boards/arduino-uno/examples/uno-millis.rs
+
+extern crate panic_halt;
+use arduino_uno::hal::port::mode::Output;
+use arduino_uno::hal::port::portb::PB5;
+use arduino_uno::prelude::*;
+use core::cell;
+
+const PRESCALER: u32 = 256;
+const TIMER_COUNTS: u32 = 250;
+
+static LED: avr_device::interrupt::Mutex<cell::RefCell<Option<PB5<Output>>>> =
+    avr_device::interrupt::Mutex::new(cell::RefCell::new(Option::None));
+
+static COUNTER: avr_device::interrupt::Mutex<cell::Cell<u32>> =
+    avr_device::interrupt::Mutex::new(cell::Cell::new(0));
+
+fn millis_init(tc0: arduino_uno::pac::TC0) {
+    // config timer for interval and enable interrupt
+    tc0.tccr0a.write(|w| w.wgm0().ctc());
+    tc0.ocr0a.write(|w| unsafe { w.bits(TIMER_COUNTS as u8) });
+    tc0.tccr0b.write(|w| match PRESCALER {
+        8 => w.cs0().prescale_8(),
+        64 => w.cs0().prescale_64(),
+        256 => w.cs0().prescale_256(),
+        1024 => w.cs0().prescale_1024(),
+        _ => panic!(),
+    });
+    tc0.timsk0.write(|w| w.ocie0a().set_bit());
+
+}
+
+#[avr_device::interrupt(atmega328p)]
+fn TIMER0_COMPA() {
+    avr_device::interrupt::free(|cs| {
+        let counter = COUNTER.borrow(cs).get();
+        COUNTER.borrow(cs).set(counter + 1);
+
+        if counter == 250 {
+            (*LED.borrow(cs).borrow_mut()).as_mut().unwrap().toggle().void_unwrap();
+            COUNTER.borrow(cs).set(0);
+        }
+
+    })
+}
+
+
+#[arduino_uno::entry]
+fn main() -> ! {
+    let peripherals = arduino_uno::Peripherals::take().unwrap();
+
+    let mut pins = arduino_uno::Pins::new(peripherals.PORTB, peripherals.PORTC, peripherals.PORTD);
+
+    millis_init(peripherals.TC0);
+
+    let led = pins.d13.into_output(&mut pins.ddr);
+    let mut serial = arduino_uno::Serial::new(
+        peripherals.USART0,
+        pins.d0,
+        pins.d1.into_output(&mut pins.ddr),
+        57600.into_baudrate(),
+    );
+    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").void_unwrap();
+    avr_device::interrupt::free(|cs| {
+        LED.borrow(cs).replace(Option::Some(led));
+        COUNTER.borrow(cs).set(0);
+    });
+
+    unsafe { avr_device::interrupt::enable() };
+
+    loop {}
+}
